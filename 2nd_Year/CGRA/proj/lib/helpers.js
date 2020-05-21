bde59406cd8b1eb7bfd3ab6b7ab90493a1e4c6a1
Telmo <44447503+Telmooo@users.noreply.github.com>
2020-05-21 17:46:35 +0100

    transfered CGRA project

--- 2nd_Year/CGRA/proj/lib/helpers.js
+++
@@ -1,7 +0,0 @@
-function hexToRgbA(hex) {
-    var match = hex.match(/^#([A-Fa-f0-9][A-Fa-f0-9])([A-Fa-f0-9][A-Fa-f0-9])([A-Fa-f0-9][A-Fa-f0-9])$/);
-    if (match)
-        return [parseInt(match[1], 16)/255.0, parseInt(match[2], 16)/255.0, parseInt(match[3], 16)/255.0, 1.0];
-    else
-        return [hex[0]/255.0, hex[1]/255.0, hex[2]/255.0, 1.0];
-}
